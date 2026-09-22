58e4b80f8d531a8786e79acfb7453afc1c75a5c2
Arthur <arthur.aubry@epitech.eu>
2026-09-22 11:24:09 +0000

    fix(import): import had wrong file name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import TodoList from './components/TodoListV2';
-
-function App() {
-    return (
-        <Container>
-            <Row>
-                <Col md={{ offset: 3, span: 6 }}>
-                    <TodoList />
-                </Col>
-            </Row>
-        </Container>
-    );
-}
-
-export default App;
+<h1>TEST VITE</h1>
