0872b5a0f9e0d9b406f2eb3c8f0b80992e4e70e5
Arthur <arthur.aubry@epitech.eu>
2026-09-22 11:25:28 +0000

    fix(file): wrong file has been pushed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,17 @@
-<h1>TEST VITE</h1>
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import TodoList from './components/TodoList';
+
+function App() {
+    return (
+        <Container>
+            <Row>
+                <Col md={{ offset: 3, span: 6 }}>
+                    <TodoList />
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default App;
